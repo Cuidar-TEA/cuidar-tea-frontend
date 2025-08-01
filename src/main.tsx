45fce262e780150929050284050a0a4e8ba5e19b
gabrieloftas <gabrieloftas@outlook.com>
2025-07-31 21:55:56 -0300

    feat: Adicao do react routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import DashboardPage from './pages/DashboardPage';
+import CadastroPage from './pages/CadastroPage';
+// import LandingPage from './pages/LandingPage';
+// import LoginPage from './pages/LoginPage';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {//<Route path="/" element={<LandingPage />} />
+        // <Route path="/login" element={<LoginPage />} />
+        }        
+        <Route path="/cadastro" element={<CadastroPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
